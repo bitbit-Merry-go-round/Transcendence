c120ffe16d2ff171bd8f98fcf5a56a4d355b9f9c
jeongyunnim <jeongyun7521@gmail.com>
2024-03-13 19:44:17 +0900

    chore: 더미데이터에 level 및 기타 유저 추가
    router.js의 user에 level과 1, 2, 3, 4, 5 유저를 추가하여 디폴트 값을 확인할 수 있도록 추가.

--- front/srcs/router.js
+++ front/srcs/router.js
@@ -10,24 +10,27 @@ const user = new ObservableObject(new User({
   profile:
     createProfile({
       id: "heshin",
+      level: 30,
       profileUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSEOCV5tCOWEb17GSCGh-mv8QfhjmWo-eIO-Go32AHeA&s" ,
     }), 
     friends: [ 
       createProfile({
         id: "jeseo",
+        level: 25,
         profileUrl: "https://ca.slack-edge.com/T039P7U66-U03M2KCK5T6-e75d6c9b8cb3-512"
       }),
-    ...["eunjiko", "hyecheon", "yham"]
+    ...["eunjiko", "hyecheon", "yham", '1', '2', '3', '4', '5']
       .map(name => createProfile({id: name}))
   ]
 }))
 
 
 export async function route() {
+  console.log(user);
   const routes = [
-    { path: "/", view: HomeView},
+    { path: "/friend", view: HomeView},
     { path: "/login", view: LoginView},
-    { path: "/friend", view: FriendView},
+    { path: "/", view: FriendView},
     { path: "/game", view: GameView },
   ]
   const match = routes.find((route) => {
@@ -41,6 +44,7 @@ export async function route() {
       user
     }
   });
+  // console.log(user);
   await page.render();
   app.appendChild(page);
   anchorToLink(app);
