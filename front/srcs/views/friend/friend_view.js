c5b5d304c044ddaca84c3b560045732247b8ef05
jeongyunnim <jeongyun7521@gmail.com>
2024-03-14 15:47:55 +0900

    feat: 각 리스트 항목 분리, 온/오프라인 토글 이벤트 추가
    각 리스트의 항목의 배경 색깔을 주어 분리했다.
    온/오프라인 배지를 list 항목을 눌러 토글할 수 있도록 했다.
    클릭 이벤트를 fetch 해온 데이터로 변경해주어야 한다.

--- front/srcs/views/friend/friend_view.js
+++ front/srcs/views/friend/friend_view.js
@@ -8,4 +8,31 @@ export default class FriendView extends View {
     super();
     this.data = data
   }
+
+  connectedCallback() {
+    super.connectedCallback();
+    // const profileCardModalBtn = document.getElementById('profileCardModalBtn');
+    // const profileCardModal = document.getElementById('profileCardModal');
+    // const modalCloseBtn = document.querySelector('.btn-close');
+    // profileCardModalBtn.addEventListener('click', () => {
+    //   profileCardModal.style.display = 'flex';
+    // });
+    // modalCloseBtn.addEventListener('click', () => {
+    //   profileCardModal.style.display = 'none';
+    // });
+    // profileCardModal.addEventListener('click', e => {
+    //   if (e.target === e.currentTarget)
+    //     profileCardModal.style.display = 'none';
+    // });
+
+    /* test on/offline */
+    const profileCardModal = document.querySelector('ul');
+    profileCardModal.addEventListener('click', e => {
+      if (e.target === e.currentTarget)
+        return ;
+      const choosedList = e.target.closest('li');
+      const statusBadge = choosedList.querySelector('.status-circle-sm');
+      statusBadge.classList.toggle('status-offline');
+    })
+  }
 }
