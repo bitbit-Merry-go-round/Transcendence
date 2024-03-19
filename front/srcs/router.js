75eae51d1e2a6f483df84ab0eb4922fc0e6e9482
bart <bartshin@icloud.com>
2024-03-19 19:23:50 +0900

    gitignore

--- front/srcs/router.js
+++ front/srcs/router.js
@@ -1,45 +1,19 @@
-import View from "@/lib/view";
 import HomeView from "@/views/home/home_view";
-import LoginView from "@/views/login/login_view";
-import GameView from "@/views/game/game_view";
-import FriendView from "@/views/friend/friend_view";
-import ModeView from "@/views/mode/mode_view";
-import RecordView from "@/views/record/record_view";
-import TournamentView from "@/views/tournament/tournament_view";
-import MatchView from "@/views/match/match_view";
-import ObservableObject from "@/lib/observable_object";
-import User, { createProfile } from "@/data/user";
+import { routes } from "@/views/viewClasses";
+import global from "@/data/global";
 
-const user = new ObservableObject(new User({
-  profile:
-    createProfile({
-      id: "heshin",
-      level: 30,
-      profileUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSEOCV5tCOWEb17GSCGh-mv8QfhjmWo-eIO-Go32AHeA&s" ,
-    }), 
-  friends: [ 
-    createProfile({
-      id: "jeseo",
-      level: 25,
-      profileUrl: "https://ca.slack-edge.com/T039P7U66-U03M2KCK5T6-e75d6c9b8cb3-512"
-    }),
-    ...["eunjiko", "hyecheon", "yham", 'test 1', 'test 2', 'test 3', 'test 4', 'test 5', 'test 6', 'test 7', 'test 8', 'test 9', 'test 10']
-      .map(name => createProfile({id: name}))
-  ],
-}))
+class Router {
 
+  static shared;
+
+  constructor() {
+    if (Router.shared) 
+      return Router.shared
+    Router.shared = this;
+  }
+}
 
 export async function route() {
-  const routes = [
-    { path: "/", view: HomeView},
-    { path: "/login", view: LoginView},
-    { path: "/friend", view: FriendView},
-    { path: "/record", view: RecordView},
-    { path: "/game", view: GameView },
-    { path: "/mode", view: ModeView },
-    { path: "/match", view: MatchView },
-    { path: "/tournament", view: TournamentView },
-  ]
   const match = routes.find((route) => {
     return route.path == location.pathname
   })
@@ -48,10 +22,9 @@ export async function route() {
   app.innerHTML = "";
   const page =  new view({
     data: {
-      user
+      user: global.user
     }
   });
-  // console.log(user);
   await page.render();
   app.appendChild(page);
   anchorToLink(app);
