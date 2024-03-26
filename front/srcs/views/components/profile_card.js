db24c2b8a2460a4d0260002064fc96d2638f54b8
jeongyunnim <jeongyun7521@gmail.com>
2024-03-26 14:01:49 +0900

    feat: 프로필 카드의 데이터를 받아올 수 있도록 함

--- front/srcs/views/components/profile_card.js
+++ front/srcs/views/components/profile_card.js
@@ -9,8 +9,7 @@ export default class ProfileCard extends View {
     super();
   }
 
-  connectedCallback() {
-    super.connectedCallback();
+  _profileCardEffect() {
     const profileCard = this.querySelector('.profile-card');
    
     profileCard.addEventListener('mousemove', e => {
@@ -30,5 +29,33 @@ export default class ProfileCard extends View {
     profileCard.addEventListener('mouseleave', function() {
         profileCard.style.transform = 'translate(-50%, -50%) perspective(1000px) rotateX(0deg) rotateY(0deg)';
     });
+  }
+
+  async _fetchInfoToProfile() {
+    const userAvatar = this.querySelector('.user-avatar');
+    const userLevel = this.querySelector('.user-level');
+    const userName = this.querySelector('.user-name');
+    const userScore = this.querySelector('.score');
+    const stateMessage = this.querySelector('.state-message');
+
+    // TODO: user를 어디엔가 저장해두어야 함.
+    // user를 임의로 변경하는 경우 어떻게 처리해주어야 할지 결정해야 함. back이냐 front냐.
+    const user = 'jeseo';
+
+    await fetch(`http://${window.location.hostname}:8000/users/${user}/profile`)
+    .then(res => res.json())
+    .then(res => {
+      userLevel.textContent = `Lv.${res.level}`;
+      userName.textContent = `${res.uid}`
+      userAvatar.src = `data:image;base64,${res.avatar}`;
+      stateMessage.textContent = `${res.message}`;
+    })
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    
+    this._profileCardEffect();
+    this._fetchInfoToProfile();
   }
 }
