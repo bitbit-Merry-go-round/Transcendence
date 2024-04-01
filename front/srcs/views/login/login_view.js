3740587518cc93687726f0d5f022ca3ecc55e0f8
jeongyunnim <jeongyun7521@gmail.com>
2024-04-01 18:49:02 +0900

    feat: JWT 토큰을 받을 수 있도록 login 버튼과 oauth 요청 연결

--- front/srcs/views/login/login_view.js
+++ front/srcs/views/login/login_view.js
@@ -15,8 +15,23 @@ export default class LoginView extends View {
 
   connectedCallback() {
     super.connectedCallback();
-    const url = `http://localhost:8000/users/42/login`;
+    // const url = `http://localhost:8000/users/42/login`;
+    const loginBtn = this.querySelector('#btn-login');
+    loginBtn.addEventListener('click', () => {
+      const FOURTYTWO_CLIENT_ID="u-s4t2ud-9c16991b58c036772092c27d7f302fb8e92f7c4635ef6f8e57d97bccbbdbfa2d";
+      const FOURTYTWO_CALLBACK_URI = `http://127.0.0.1:8000/users/42/callback`;
+      const url = `https://api.intra.42.fr/oauth/authorize`;
+      const body = JSON.stringify({
+        client_id: FOURTYTWO_CLIENT_ID,
+        redirect_uri: FOURTYTWO_CALLBACK_URI,
+        response_type: 'code'
+      })
 
-    httpRequest('GET', url, null, this._getJWT);
+      // window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${FOURTYTWO_CLIENT_ID}&redirect_uri=${FOURTYTWO_CALLBACK_URI}&response_type=code`;
+      window.location.href = `http://localhost:8000/users/42/login`;
+      // httpRequest('GET', url, null, this._getJWT);
+
+    })
+
   }
 }
