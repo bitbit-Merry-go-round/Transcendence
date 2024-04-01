2ad19e9ed2aed44713d0463ae23c6c13e679417e
jeongyunnim <jeongyun7521@gmail.com>
2024-04-01 20:35:18 +0900

    feat: OAuth 플로우의 일부 로직을 front side로 옮김

--- front/srcs/views/login/login_view.js
+++ front/srcs/views/login/login_view.js
@@ -15,23 +15,22 @@ export default class LoginView extends View {
 
   connectedCallback() {
     super.connectedCallback();
-    // const url = `http://localhost:8000/users/42/login`;
-    const loginBtn = this.querySelector('#btn-login');
-    loginBtn.addEventListener('click', () => {
-      const FOURTYTWO_CLIENT_ID="u-s4t2ud-9c16991b58c036772092c27d7f302fb8e92f7c4635ef6f8e57d97bccbbdbfa2d";
-      const FOURTYTWO_CALLBACK_URI = `http://127.0.0.1:8000/users/42/callback`;
-      const url = `https://api.intra.42.fr/oauth/authorize`;
-      const body = JSON.stringify({
-        client_id: FOURTYTWO_CLIENT_ID,
-        redirect_uri: FOURTYTWO_CALLBACK_URI,
-        response_type: 'code'
+
+    const queryString = window.location.search;
+    if (queryString)
+    {
+      const authCode = new URLSearchParams(queryString).get('code');
+      console.log('code', authCode);
+      const uri = `http://127.0.0.1:8000/users/42/callback?code=${authCode}`;
+
+      httpRequest('GET', uri, null, this._getJWT);
+    }
+    else
+    {
+      const loginBtn = this.querySelector('#btn-login');
+      loginBtn.addEventListener('click', () => {
+        window.location.href = `http://localhost:8000/users/42/login`;
       })
-
-      // window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${FOURTYTWO_CLIENT_ID}&redirect_uri=${FOURTYTWO_CALLBACK_URI}&response_type=code`;
-      window.location.href = `http://localhost:8000/users/42/login`;
-      // httpRequest('GET', url, null, this._getJWT);
-
-    })
-
+    }
   }
 }
