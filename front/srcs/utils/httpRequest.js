4e64788837981c932f599c90a755be537a0dcf49
jeongyunnim <jeongyun7521@gmail.com>
2024-04-02 18:01:37 +0900

    fix: httpRequest 리팩토링
    - json()이 프로미스를 반환하고 있어 catch로 에러처리를 할 수 있도록 변환

--- front/srcs/utils/httpRequest.js
+++ front/srcs/utils/httpRequest.js
@@ -13,40 +13,50 @@ function fetch_failed(url) {
  * @param {function} fail
 */
 export default function httpRequest(method, url, body, success, fail = fetch_failed) {
+    const headers = {
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+        "Content-Type": "application/json"
+    };
+    console.log('header: ', headers);
+
     fetch(url, {
         method: method,
-        headers: {
-            Authorization: "Bearer " + localStorage.getItem("access_token"),
-            "Content-Type": "application/json"
-        },
+        headers: headers,
         body: body
     })
     .then((res) => {
         if (res.status === 200 || res.status == 201){
-            return success(res.json());
+            return res.json();
         }
         const refresh_token = localStorage.getItem("refresh_token")
         // access_token 이 만료되어 권한이 없고, 리프레시 토큰이 있다면 그 리프레시 토큰을 이용해서 새로운 access token 을 요청
         if (res.status === 401 && refresh_token) {
-            const GET_TOKEN_URI = `http://${window.location.hostname}:8000/api/token`;
+            const GET_TOKEN_URI = `http://${window.location.hostname}:8000/refresh`;
+
             fetch(GET_TOKEN_URI, {
                 method: "POST",
-                headers: {
-                    Authorization: "Bearer " + localStorage.getItem("access_token"),
-                    "Content-Type": "application/json"
-                },
+                headers: headers,
                 body: JSON.stringify({
                     refresh_token
                 })
-            }).then((res) => {
-                return res.json()
-            }).then((result) => {
+            })
+            .then((res) => res.json())
+            .then((result) => {
                 localStorage.setItem("access_token", result.accessToken)
                 httpRequest(method, url, body, success, fail)
-            });
+            })
+            .catch(() => {
+                fail(url);
+            })
         }
         else {
-            return fail(url);
+            throw  new Error('failed to refresh token.');
         }
+    })
+    .then((json) => {
+        success(json);
+    })
+    .catch(() => {
+        fail(url);
     })
 }
