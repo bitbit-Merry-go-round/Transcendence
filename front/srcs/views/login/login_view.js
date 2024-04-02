ec7156ac669afc3a7f01c0acc948d1fc469d6144
jeongyunnim <jeongyun7521@gmail.com>
2024-04-02 14:04:47 +0900

    feat: Resource owner 측에서 42로 직접 요청하도록 변경 (#52)

--- front/srcs/views/login/login_view.js
+++ front/srcs/views/login/login_view.js
@@ -29,7 +29,9 @@ export default class LoginView extends View {
     {
       const loginBtn = this.querySelector('#btn-login');
       loginBtn.addEventListener('click', () => {
-        window.location.href = `http://localhost:8000/users/42/login`;
+        const client_id = 'u-s4t2ud-9c16991b58c036772092c27d7f302fb8e92f7c4635ef6f8e57d97bccbbdbfa2d';
+        const redirect_uri = 'http%3A%2F%2F127.0.0.1%3A8080%2Flogin';
+        window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
       })
     }
   }
