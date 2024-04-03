fa3f548bb36f152964ce6e9518927af4d7d8b172
jeongyunnim <jeongyun7521@gmail.com>
2024-04-03 16:25:48 +0900

    feat: Input 값 유효하지 않은 경우 사용자에게 보여줄 동작 추가

--- front/srcs/views/auth/auth_view.js
+++ front/srcs/views/auth/auth_view.js
@@ -17,16 +17,51 @@ export default class AuthView extends View {
       }
     });
     otpInput.addEventListener('keypress', (e) => {
-      const {value} = e.target;
-      if (value.length >= 5)
+      if (e.target.value.length >= 5)
       {
           e.preventDefault();
       }
     });
   }
 
+  _failToAuthHandler() {
+    const otpInput = this.querySelector('#tfa');
+    const submitBtn = this.querySelector('#btn-tfa');
+    const errorPassage = this.querySelector('#otp-error');
+    
+    errorPassage.style.display = 'flex';
+    otpInput.classList.add(`vibration`);
+    setTimeout(() => {
+      otpInput.classList.remove("vibration");
+      errorPassage.style.display = 'none';
+      submitBtn.removeAttribute('disabled');
+    }, 700);
+  }
+
+  async _submitBtnControl() {
+    const otpInput = this.querySelector('#tfa');
+    const submitBtn = this.querySelector('#btn-tfa');
+    const url = `http://localhost:8000/2fa`
+    const {value} = otpInput;
+
+    submitBtn.addEventListener('click', async (e) => {
+      console.log(e.target.value);
+      submitBtn.setAttribute('disabled', '');
+      if (otpInput.value.length < 5)
+      {
+        this._failToAuthHandler();
+        return ;
+      }
+      await httpRequest('POST', url, value, (res) => {
+        console.log('2fa 인증 성공');
+        //move to home logic 옮겨야 함.
+      }, this._failToAuthHandler.bind(this));
+    })
+  }
+
   connectedCallback() {
     super.connectedCallback();
     this._otpInputControl();
+    this._submitBtnControl();
   }
 }
