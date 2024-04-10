cdafa103f7c6222270617edd107d8818d9963da7
bart <bartshin@icloud.com>
2024-04-10 20:12:51 +0900

    fix: login url

--- front/srcs/index.js
+++ front/srcs/index.js
@@ -15,8 +15,10 @@ document.addEventListener("DOMContentLoaded", async () => {
   const history = window.history.state?.history;
   const index = window.history.state?.index;
 
-  if (!localStorage.getItem("access") && !DEBUG.isDebug())
-  path = "/login";
+  if (!path.includes("login") &&
+  !localStorage.getItem("access") && !DEBUG.isDebug()) {
+    path = "/login";
+  }
 
   if (!DEBUG.isDebug() && 
     (!history || index == undefined)) {
