a67ff605ae554e8569bfc259cbdc95eb509fbce8
jeongyunnim <jeongyun7521@gmail.com>
2024-04-12 14:43:06 +0900

    fix: 모드 뷰 뒤로가기 변경

--- front/srcs/views/mode/mode_view.js
+++ front/srcs/views/mode/mode_view.js
@@ -1,4 +1,5 @@
 import View from "@/lib/view";
+import { NAVIGATE_DRIRECTION, route } from "@/router";
 
 export default class ModeView extends View {
 
@@ -11,6 +12,14 @@ export default class ModeView extends View {
 
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    const backBtn = document.getElementById('move-to-home');
+    backBtn.addEventListener('click', () => {
+      route({
+        path: '/home',
+        direction: NAVIGATE_DRIRECTION.backward
+      })
+    })
   }
 
 }
