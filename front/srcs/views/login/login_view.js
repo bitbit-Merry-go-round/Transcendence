c8e32bc6fa8eb255e2f14fb6ead344a1c79be340
jeongyunnim <jeongyun7521@gmail.com>
2024-04-15 13:35:05 +0900

    chore: 고정 아이피 변경

--- front/srcs/views/login/login_view.js
+++ front/srcs/views/login/login_view.js
@@ -14,7 +14,7 @@ export default class LoginView extends View {
     const loginBtn = this.querySelector('#btn-login');
     loginBtn.addEventListener('click', () => {
       const client_id = 'u-s4t2ud-c2f2e5bdcebfdc16951b04539fe1cb50ab27565e22cdc3aaf0c3a2d33c33a9ee';
-      const redirect_uri = 'https%3A%2F%2F10.14.4.1%2Flogin';
+      const redirect_uri = 'https%3A%2F%2F10.32.6.2%2Flogin';
       window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
     })
   }
